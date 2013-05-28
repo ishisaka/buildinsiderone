fb3177a567a1d3dbbdd31c100f8370140400d677
Tadahiro Ishisaka <ishisaka@gmail.com>
2013-05-28 21:18:29 +0900

    WinFormプロジェクトの追加

---
+++ buildinsiderone/BuildInsider.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace buildinsiderone
+{
+    partial class BuildInsider
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.MessageButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MessageButton
+            // 
+            this.MessageButton.Location = new System.Drawing.Point(258, 53);
+            this.MessageButton.Name = "MessageButton";
+            this.MessageButton.Size = new System.Drawing.Size(102, 37);
+            this.MessageButton.TabIndex = 0;
+            this.MessageButton.Text = "( ノﾟДﾟ)ﾖｯ!";
+            this.MessageButton.UseVisualStyleBackColor = true;
+            this.MessageButton.Click += new System.EventHandler(this.MessageButton_Click);
+            // 
+            // BuildInsider
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 102);
+            this.Controls.Add(this.MessageButton);
+            this.Name = "BuildInsider";
+            this.Text = "BuildInsider";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button MessageButton;
+    }
+}
+
---
+++ buildinsiderone/BuildInsider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace buildinsiderone
+{
+    public partial class BuildInsider : Form
+    {
+        public BuildInsider() {
+            InitializeComponent();
+        }
+
+        private void MessageButton_Click(object sender, EventArgs e) {
+            MessageBox.Show("第1回 BuildInsider OFFLINEにようこそ");
+        }
+    }
+}
